f792e6ae684bbf00c53d5ecac891937f1156430f
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-07-26 23:49:19 +1200

    change to str enum

--- src/function/enums.py
+++ src/function/enums.py
@@ -1,7 +1,7 @@
-from enum import Enum
+from enum import StrEnum
 
 
-class FunctionEndpoints(Enum):
+class FunctionEndpoints(StrEnum):
     """Default endpoints for functions"""
 
     HEALTH = "/healthz"
