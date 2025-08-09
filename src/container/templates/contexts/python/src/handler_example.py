006402a6ca3c866c5cf0937bbfcb08c35f7c4f10
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-08-09 14:04:57 +1200

    Rename handler to avoid collision with user code

---
+++ src/container/templates/contexts/python/src/handler_example.py
@@ -0,0 +1,18 @@
+# This template is overwritten by the user input
+from runtime_models import Context, Event, Response
+
+
+def handler(event: Event, ctx: Context):
+    """
+    This function is the entry point for the function.
+    It will be invoked by the FaaS platform.
+    """
+
+    # User logic: can return dict (JSON), (dict, status), or Response()
+    return {
+        "message": "Hello World",
+        "function_id": ctx.function_id,
+        "request_id": ctx.request_id,
+        "path": event.path,
+        "contract": ctx.contract_version,
+    }
