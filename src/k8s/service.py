3cafaa36deb46b9434a6b0cb8cd98604144e46e1
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-08-12 08:49:57 +1200

    Refactor k8s module to di for k8s clients

--- src/k8s/service.py
+++ src/k8s/service.py
@@ -1,32 +1,14 @@
 import time
 import logging
 from kubernetes import client
-from src.config import config
 
 from .exceptions import PodTimeoutError
 
 log = logging.getLogger(__name__)
 
 
-def get_k8s_custom_objects_client() -> client.CustomObjectsApi:
-    """Get the CustomObjectsApi client with proper config"""
-    return client.CustomObjectsApi(config.get_k8s_client())
-
-
-def get_k8s_core_client() -> client.CoreV1Api:
-    """Get the CoreV1Api client with proper config"""
-    return client.CoreV1Api(config.get_k8s_client())
-
-
-def get_k8s_api_client() -> client.ApiClient:
-    """Get the ApiClient with the proper config"""
-    # TODO: update the client configuration
-    return client.ApiClient()
-
-
-def get_knative_route(name: str, namespace: str):
+def get_knative_route(client: client.CustomObjectsApi, name: str, namespace: str):
     """Get the endpoint of an knative service"""
-    client = get_k8s_custom_objects_client()
     return client.get_namespaced_custom_object(
         group="serving.knative.dev",
         version="v1",
@@ -36,11 +18,12 @@ def get_knative_route(name: str, namespace: str):
     )
 
 
-def wait_for_succeeded(name: str, namespace: str, timeout: int):
+def wait_for_succeeded(
+    client: client.CoreV1Api, name: str, namespace: str, timeout: int
+):
     """Waits for 'Succeeded' status from pod. Raises on timeout with logs"""
 
     # Could early exit on Error status
-    client = get_k8s_core_client()
     t_start = time.perf_counter()
 
     while True:
