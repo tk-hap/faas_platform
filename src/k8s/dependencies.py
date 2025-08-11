a8e46bcd582af1f39630fd4c1a18751dfd606dba
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-08-12 08:56:24 +1200

    Rename to match code style

--- src/k8s/dependencies.py
+++ src/k8s/dependencies.py
@@ -2,17 +2,17 @@ from kubernetes import client
 from src.config import config
 
 
-def get_k8s_custom_objects_client() -> client.CustomObjectsApi:
+def k8s_custom_objects_client() -> client.CustomObjectsApi:
     """Get the CustomObjectsApi client with proper config"""
     return client.CustomObjectsApi(config.get_k8s_client())
 
 
-def get_k8s_core_client() -> client.CoreV1Api:
+def k8s_core_client() -> client.CoreV1Api:
     """Get the CoreV1Api client with proper config"""
     return client.CoreV1Api(config.get_k8s_client())
 
 
-def get_k8s_api_client() -> client.ApiClient:
+def k8s_api_client() -> client.ApiClient:
     """Get the ApiClient with the proper config"""
     # TODO: update the client configuration
     return client.ApiClient()
